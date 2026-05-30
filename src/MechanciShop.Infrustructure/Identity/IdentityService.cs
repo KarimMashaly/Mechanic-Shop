45b61ea2cb183b4af3430664741df461f6643f4f
karim <karimmashaly9@gmail.com>
2026-05-30 22:35:48 +0300

    implement IIdentityService interface

---
+++ src/MechanciShop.Infrustructure/Identity/IdentityService.cs
@@ -0,0 +1,46 @@
+﻿using MechanicShop.Application.Common.Interfaces;
+using MechanicShop.Application.Features.Identity.Dtos;
+using MechanicShop.Domain.Common.Results;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace MechanciShop.Infrustructure.Identity
+{
+    public class IdentityService(
+        UserManager<AppUser> userManager,
+        IUserClaimsPrincipalFactory<AppUser> userClaimsPrincipalFactory,
+        IAuthorizationService authorizationService) : IIdentityService
+    {
+        private readonly UserManager<AppUser> _userManager = userManager;
+        private readonly IUserClaimsPrincipalFactory<AppUser> _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+        private readonly IAuthorizationService _authorizationService = authorizationService;
+
+        public async Task<Result<AppUserDto>> AuthenticateAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if(user is null)
+                return Error.NotFound("User_Not_Found", $"User with email {UtilityService.MaskEmail(email)} not found");
+
+            if(!user.EmailConfirmed)
+                return Error.Conflict("Email_Not_Confirmed", $"email '{UtilityService.MaskEmail(email)}' not confirmed");
+
+            if(!await _userManager.CheckPasswordAsync(user, password))
+                return Error.Conflict("Invalid_Login_Attempt", "Email / Password are incorrect");
+
+            return new AppUserDto(user.Id, user.Email!, await _userManager.GetRolesAsync(user), await _userManager.GetClaimsAsync(user));
+        }
+
+        public async Task<Result<AppUserDto>> GetUserByIdAsync(string UserId)
+        {
+            var user = await _userManager.FindByIdAsync(UserId) ?? throw new InvalidOperationException(nameof(UserId));
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            return new AppUserDto(UserId, user.Email!, roles, claims);
+        }
+    }
+}
