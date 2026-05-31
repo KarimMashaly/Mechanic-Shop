79306cfb71915d570dd49950b5c33ce3e915ba70
karim <karimmashaly9@gmail.com>
2026-05-31 17:59:32 +0300

    impelement Dashboard controller

---
+++ src/MechanicShop.Api/Controllers/DashboradController.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Versioning;
+
+using MechanicShop.Application.Features.Dashboard.Dtos;
+using MechanicShop.Application.Features.Dashboard.Queries.GetWorkOrderStates;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MechanicShop.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/dashboard")]
+    [ApiVersion("1.0")]
+    [Authorize]
+    public sealed class DashboradController(ISender sender) : ApiController
+    {
+        [HttpGet("stats")]
+        [ProducesResponseType(typeof(TodayWorkOrderStatsDto), statusCode: StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), statusCode: StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult>GetTodayStats(DateOnly? date, CancellationToken ct)
+        {
+            var statsDate = date ?? DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var result = await sender.Send(new GetWorkOrderStatsQuery(statsDate), ct);
+
+            return result.Match(
+                response => Ok(response),
+                Problem);
+        }
+    }
+}
