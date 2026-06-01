179cbc4374443d63b420da116b239dc7f6061655
karim <karimmashaly9@gmail.com>
2026-06-01 07:36:48 +0300

    implement repair task request models

---
+++ src/MechanicShop.Contracts/Requests/RepairTasks/CreateRepairTaskPartRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MechanicShop.Contracts.Requests.RepairTasks
+{
+    public class CreateRepairTaskPartRequest
+    {
+        [Required(ErrorMessage = "Part name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Quantity is required.")]
+        [Range(1, 10, ErrorMessage = "Quantity must be between 1 and 10.")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Cost is required.")]
+        [Range(0, 10000, ErrorMessage = "Cost must be between 1 and 10000.")]
+        public decimal Cost { get; set; }
+    }
+}
---
+++ src/MechanicShop.Contracts/Requests/RepairTasks/UpdateRepairTaskRequest.cs
@@ -0,0 +1,22 @@
+﻿using MechanicShop.Contracts.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace MechanicShop.Contracts.Requests.RepairTasks
+{
+    public class UpdateRepairTaskRequest
+    {
+        [Required(ErrorMessage = "Task name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Cost is required.")]
+        [Range(1, 10000, ErrorMessage = "Cost must be between 1 and 10,000.")]
+        public decimal LaborCost { get; set; }
+
+        [Required(ErrorMessage = "Estimated duration is requierd.")]
+        public RepairDurationInMinutes EstimatedDurationInMins { get; set; }
+
+        [MinLength(1, ErrorMessage = "At least one part is required.")]
+        public List<UpdateRepairTaskPartRequest> Parts { get; set; } = [];
+    }
+
+}
