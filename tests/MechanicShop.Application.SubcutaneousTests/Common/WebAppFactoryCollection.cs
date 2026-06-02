ab1b3ff8bd3acbc375875640d7f1af0c5c2e3484
karim <karimmashaly9@gmail.com>
2026-06-02 08:39:57 +0300

    add WebAppFactoryCollection class

---
+++ tests/MechanicShop.Application.SubcutaneousTests/Common/WebAppFactoryCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace MechanicShop.Application.SubcutaneousTests.Common
+{
+    [CollectionDefinition(CollectionName)]
+    public class WebAppFactoryCollection : ICollectionFixture<WebAppFactory>
+    {
+        public const string CollectionName = "WebAppFactoryCollection";
+    }
+}
