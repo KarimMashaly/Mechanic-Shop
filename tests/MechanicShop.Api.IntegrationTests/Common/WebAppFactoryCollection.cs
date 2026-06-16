d7a86f88e64e16d5ab56b79a59bb86e647935233
karim <karimmashaly9@gmail.com>
2026-06-16 15:12:50 +0300

    add web app factory collection

---
+++ tests/MechanicShop.Api.IntegrationTests/Common/WebAppFactoryCollection.cs
@@ -0,0 +1,11 @@
+﻿using MechanicShop.Api.IntegrationTests.Common;
+using Xunit;
+
+namespace MechanicShop.Api.IntegrationTests
+{
+    [CollectionDefinition(CollectionName)]
+    public class WebAppFactoryCollection : ICollectionFixture<WebAppFactory>
+    {
+        public const string CollectionName = "WebAppFactoryCollection";
+    }
+}
